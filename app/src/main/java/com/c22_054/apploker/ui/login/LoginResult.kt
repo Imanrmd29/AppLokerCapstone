35b45c46cd5eb4187953d3356acaf178edf1c689
Imanrmd29 <iman.ramadhan@students.utdi.ac.id>
2022-12-07 23:15:22 +0700

    Menambahkan Layout Login dan resource lainya

---
+++ app/src/main/java/com/c22_054/apploker/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.c22_054.apploker.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
